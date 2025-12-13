dfd76859382dc8d0431295fdce2253070d7bac31
howardsolutions <phungthinh0946519356@gmail.com>
2025-12-13 22:02:22 +0700

    chore: setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@workspace/ui/components/button"
+import { add } from '@workspace/math/add';
+import { Button } from '@workspace/ui/components/button';
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+    <div className='flex items-center justify-center min-h-svh'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-2xl font-bold'>Hello from apps/web</h1>
+        <Button size='sm'>Button</Button>
+        <p>{add(3, 4)}</p>
       </div>
     </div>
-  )
+  );
 }
