128922cac41ae2752b9dd2d1e4a3ec1e715d69c1
howardsolutions <phungthinh0946519356@gmail.com>
2025-12-15 16:50:37 +0700

    feat: setup convex backend

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@workspace/ui/components/button"
+import { api } from '@workspace/backend/_generated/api';
+import { useQuery } from 'convex/react';
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello from apps/widget</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className='flex flex-col items-center justify-center min-h-svh'>
+      <h1 className='text-2xl font-bold max-w-sm w-full mx-auto gap-y-4'>
+        Hello from apps/widget
+      </h1>
+      {JSON.stringify(users)}
     </div>
-  )
+  );
 }
