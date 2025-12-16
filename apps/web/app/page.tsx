5c6ee39d50675588e49c97bf45e0b5c908059de6
howardsolutions <phungthinh0946519356@gmail.com>
2025-12-16 16:00:43 +0700

    feat: integrate convex & clerk for authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,26 @@
-import { api } from '@workspace/backend/_generated/api';
-import { useQuery } from 'convex/react';
+'use client';
+
+import { SignInButton, UserButton } from '@clerk/nextjs';
+import { Authenticated, Unauthenticated } from 'convex/react';
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
+  // const users = useQuery(api.users.getMany);
+
   return (
-    <div className='flex flex-col items-center justify-center min-h-svh'>
-      <h1 className='text-2xl font-bold max-w-sm w-full mx-auto gap-y-4'>
-        Hello from apps/web
-      </h1>
-      {JSON.stringify(users)}
+    <div>
+      <Authenticated>
+        <div className='flex flex-col items-center justify-center min-h-svh'>
+          <h1 className='text-2xl font-bold max-w-sm w-full mx-auto gap-y-4'>
+            Hello from apps/web
+          </h1>
+          <UserButton />
+        </div>
+      </Authenticated>
+
+      <Unauthenticated>
+        <p>Must be signed in</p>
+        <SignInButton>Sign In!</SignInButton>
+      </Unauthenticated>
     </div>
   );
 }
