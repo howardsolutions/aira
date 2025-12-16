adcdab67a57be7ea1df97716c257aafb0e17b07e
howardsolutions <phungthinh0946519356@gmail.com>
2025-12-16 16:30:22 +0700

    feat: custom signup, signin page with clerk

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className='min-h-screen min-w-screen flex flex-col items-center justify-center'>
+      {children}
+    </div>
+  );
+}
+
+export default AuthLayout;
