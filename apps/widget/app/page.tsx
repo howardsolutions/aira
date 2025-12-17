2d54bc5d03b003a92df200e325e458cb645b12aa
howardsolutions <phungthinh0946519356@gmail.com>
2025-12-17 16:28:41 +0700

    refactor: the widget main page to client component to ultilize the vapi hook

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,31 @@
-import { api } from '@workspace/backend/_generated/api';
-import { useQuery } from 'convex/react';
+'use client';
+
+import { useVapi } from '@/modules/widget/hooks/use-vapi';
+import { Button } from '@workspace/ui/components/button';
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
+  const {
+    isSpeaking,
+    isConnecting,
+    isConnected,
+    transcript,
+    startCall,
+    endCall,
+  } = useVapi();
+
   return (
     <div className='flex flex-col items-center justify-center min-h-svh'>
-      <h1 className='text-2xl font-bold max-w-sm w-full mx-auto gap-y-4'>
-        Hello from apps/widget
-      </h1>
-      {JSON.stringify(users)}
+      <Button onClick={() => startCall()}>Start Call</Button>
+
+      <Button variant={'destructive'} onClick={() => endCall()}>
+        Stop Call
+      </Button>
+
+      <p>isConnected: {isConnected}</p>
+      <p>isConnecting: {isConnecting}</p>
+      <p>isSpeaking: {isSpeaking}</p>
+
+      <p>{JSON.stringify(transcript, null, 2)}</p>
     </div>
   );
 }
