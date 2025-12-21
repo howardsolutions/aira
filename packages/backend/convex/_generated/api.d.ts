66fc9e8d4ce148ce2428b993fd835d3992c8fd21
howardsolutions <phungthinh0946519356@gmail.com>
2025-12-21 21:50:44 +0700

    feat: widget session

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as public_contacctSession from "../public/contacctSession.js";
 import type * as users from "../users.js";
 
 import type {
@@ -17,6 +18,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  "public/contacctSession": typeof public_contacctSession;
   users: typeof users;
 }>;
 
